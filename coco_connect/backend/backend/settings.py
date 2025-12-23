15ec1166160e25897238df7ef2d390f995265e8c
SakithmaVorandi <vorandi.uog04@edu.lnbti.lk>
2025-12-23 23:50:54 +0530

    updates

--- coco_connect/backend/backend/settings.py
+++ coco_connect/backend/backend/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
@@ -27,9 +26,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
-
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -44,12 +41,9 @@ INSTALLED_APPS = [
 
     "blockchain_records",
 
-
     # your app
     'connect',
-
     'rest_framework_simplejwt',
-
 ]
 
 MIDDLEWARE = [
@@ -82,7 +76,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'backend.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
@@ -96,9 +89,6 @@ DATABASES = {
         'PORT': '5432',
     }
 }
-
-
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
