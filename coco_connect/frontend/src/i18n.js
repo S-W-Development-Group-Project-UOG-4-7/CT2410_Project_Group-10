d4ba544fef9b4197d6dbb431115c2a47ac1effc2
SakithmaVorandi <vorandi.uog04@edu.lnbti.lk>
2026-01-21 16:19:42 +0530

    Lang button update

---
+++ coco_connect/frontend/src/i18n.js
@@ -0,0 +1,23 @@
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+
+import en from "./locales/en.json";
+import si from "./locales/si.json";
+import ta from "./locales/ta.json";
+
+const savedLang = localStorage.getItem("lang") || "en";
+
+i18n
+  .use(initReactI18next)
+  .init({
+    resources: {
+      en: { translation: en },
+      si: { translation: si },
+      ta: { translation: ta },
+    },
+    lng: savedLang,
+    fallbackLng: "en",
+    interpolation: { escapeValue: false },
+  });
+
+export default i18n;
